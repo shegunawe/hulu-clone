99b0717fbdd463a7b9da708cc00aed97b59d2239
shegunawe <74549078+shegunawe@users.noreply.github.com>
2022-07-10 19:40:40 +0100

    commit

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,10 +1,10 @@
 import Thumbnail from "./Thumbnail";
 import FlipMove from "react-flip-move";
 
-export default function Results({ results }) {
+export default function Results({ result }) {
   return (
     <FlipMove className="text-gray-200 px-5 my-10 select-none sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-      {results.map((result) => (
+      {result?.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
     </FlipMove>
--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({results}) {
       
       <Header />
       <Navbar />
-      <Results results={ results} />
+      <Results result={ results} />
     </div>
   )
 }
