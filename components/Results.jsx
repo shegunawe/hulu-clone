891d5f4c2769d60c63c030d982a50690c399db51
shegunawe <74549078+shegunawe@users.noreply.github.com>
2022-07-10 18:50:52 +0100

    fix reading error

--- components/Results.jsx
+++ components/Results.jsx
@@ -4,7 +4,7 @@ import FlipMove from "react-flip-move";
 export default function Results({ results }) {
   return (
     <FlipMove className="text-gray-200 px-5 my-10 select-none sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-      {results.map((result) => (
+      {results?.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
     </FlipMove>
